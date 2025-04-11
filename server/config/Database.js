805720895fc5e7e5b8175fb2f92b097372cb5c0b
krishna-dev11 <krishnagothwal0@gmail.com>
2025-04-12 00:45:39 +0530

    another bug fix

--- server/config/Database.js
+++ server/config/Database.js
@@ -3,8 +3,6 @@ require("dotenv").config();
 
 exports.dbconnect = ()=>{
     mongoose.connect(process.env.MONGODB_URL, {
-        useNewUrlParser : true,
-        useUnifiedTopology : true
     })
     .then(()=>{console.log("Database connetion held successfully")})
     .catch((error)=>{
