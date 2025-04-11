741a1eecf13ac5991f6bc5746717846600b6c29a
krishna-dev11 <krishnagothwal0@gmail.com>
2025-04-12 00:09:58 +0530

    bug fix

--- server/Models/category.js
+++ server/Models/category.js
@@ -20,3 +20,31 @@ const categorySchema = new mongoose.Schema({
 {timestamps : true});
 
 module.exports = mongoose.model("category" , categorySchema);
+
+
+
+
+// const mongoose = require("mongoose");
+
+// const courseProgressSchema = new mongoose.Schema(
+//   {
+//     courseId: {
+//       type: mongoose.Schema.Types.ObjectId,
+//       ref: "courses", 
+//       required: true,
+//     },
+//     userId: {
+//       type: mongoose.Schema.Types.ObjectId,
+//       ref: "user",
+//     },
+//     completedVideos: [
+//       {
+//         type: mongoose.Schema.Types.ObjectId,
+//         ref: "subSection",
+//       },
+//     ],
+//   },
+//   { timestamps: true }
+// );
+
+// module.exports = mongoose.model( "courseprogress" , courseProgressSchema);
