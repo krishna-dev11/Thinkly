ce7addb52a9660132f61bd900ea1df8dda70059b
krishna-dev11 <krishnagothwal0@gmail.com>
2025-04-30 22:10:01 +0530

    finall code added

--- src/Pages/LiveClass.jsx
+++ src/Pages/LiveClass.jsx
@@ -8,6 +8,7 @@ const LiveClass = () => {
     const {RoomId} = useParams()
     const {user} = useSelector(state=>state.profile)
     const navigate = useNavigate()
+    console.log(user)
 
     const MyMeeting = async(element)=>{
      const appId = 1856231876 ;
